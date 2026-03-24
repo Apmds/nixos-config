1548337827026173eec6fee7ff5df0a6c57c2f8f
Apmds <ap1mrfj@gmail.com>
2026-03-24 16:40:53 +0000

    remove spicetify flake stuff from home

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,17 +1,4 @@
 { config, pkgs, ... }:
-
-let
-  # 1. Pull in flake-compat to translate the flake outputs for our non-flake setup
-  flake-compat = import (fetchTarball "https://github.com/edolstra/flake-compat/archive/master.tar.gz");
-  
-  # 2. Use it to evaluate spicetify-nix
-  spicetify-flake = (flake-compat {
-    src = fetchTarball "https://github.com/Gerg-L/spicetify-nix/archive/master.tar.gz";
-  }).defaultNix;
-
-  # 3. Target your specific system architecture for the packages
-  spicePkgs = spicetify-flake.legacyPackages.${pkgs.stdenv.hostPlatform.system};
-in
 {
   imports = [
     ./gtk.nix
