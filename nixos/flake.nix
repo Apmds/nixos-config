3edc2dfed1aacd05c54ea67df4cd537182ca66f6
Apmds <ap1mrfj@gmail.com>
2026-06-05 18:45:12 +0100

    network_manager_ui flake input

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,11 +3,16 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    network_manager_ui = {
+      url = "github:Blazzzeee/network_manager_ui?dir=nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, network_manager_ui }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      specialArgs = { inherit network_manager_ui; };
       modules = [ ./configuration.nix ];
     };
   };
